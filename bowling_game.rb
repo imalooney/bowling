07494784fc8474bb028a134c9ad72564d1efab3a
Tejas Rawal <trawal90@gmail.com>
2013-11-21 11:18:40 -0600

    update roll method to push pins to array and update score method for accurate score when no frames have spare or strike

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,13 +5,20 @@ class BowlingGame
 		@rolls = []
 	end
 
+# Record a roll in the game.
+# 
+# pins - The Integer number of pins knocked down in this roll.
+# 
+# Returns nothing.
 	def roll(pins)
+		@rolls << pins
 	end	
 
 # Returns the Integer score for this game.
 # Expects to be run after all rolls for the game have been recorded.
 	def score
-		0
+		# Will record score accuratly if no frames contain spare or strike
+		@rolls.reduce(:+)
 	end	
 
 end
