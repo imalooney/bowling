d72f691a66d5068914a8f19f4b5fd68c4de517e5
Tejas Rawal <trawal90@gmail.com>
2013-11-21 11:03:47 -0600

    add failing test for 0 score and add to bowling_game for passing test

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,14 +5,23 @@ require 'rspec'
 require_relative '../bowling_game'
 
 describe BowlingGame do
+
+	let(:game) { BowlingGame.new }
+
 	describe '.initialize' do
 		it 'sets @rolls to an empty array' do
-			game = BowlingGame.new
 
-			expect(game.rolls).to eq ([])
-			
+			expect(game.rolls).to eq ([])		
 		end
+	end
 
+	describe '#score' do
+		it 'scores the game as 0 when only gutter balls are thrown' do
+
+			20.times { game.roll(0) }
+
+			expect(game.score).to eq(0)
+		end
 	end
 
 end
